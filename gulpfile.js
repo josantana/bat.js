a6d21c96350571d36914ddc8f65af319f994acf0
Jo Santana <josantana@jogalabs.com>
2014-10-16 14:41:03 -0300

    Moved "bat.js" to src folder
    - Also included header info

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,32 @@
-var gulp         = require('gulp');
-var concat       = require('gulp-concat');
-var uglify       = require('gulp-uglify');
-var notify       = require("gulp-notify");
+var gulp    = require('gulp');
+var concat  = require('gulp-concat');
+var uglify  = require('gulp-uglify');
+var header  = require('gulp-header');
+var notify  = require("gulp-notify");
 
 var js = [ 'src/bat.*.js' ];
+
+var pkg = require('./package.json');
+var jsInfo =
+[
+    '/*!',
+    ' * <%= pkg.name %> - <%= pkg.description %>',
+    ' * @author <%= pkg.author %>',
+    ' * @version v<%= pkg.version %>',
+    ' * @link <%= pkg.url %>',
+    ' * @license <%= pkg.license %>',
+    ' */',
+    '',
+    ''
+].join('\n');
 
 gulp.task('scripts', function()
 {
   return gulp.src(js)
   .pipe(concat('bat.js'))
   .pipe(uglify())
-  .pipe(gulp.dest('./'))
+  .pipe(header(jsInfo, { pkg: pkg }))
+  .pipe(gulp.dest('src'))
   .pipe(notify('Main bat.js file was created.'));
 });
 
