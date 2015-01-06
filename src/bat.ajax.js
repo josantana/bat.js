5a9944bdd0944c190063749fc00a10c5b43a1fbe
Jo Santana <josantana@jogalabs.com>
2015-01-06 18:00:15 -0200

    Ajax Module Update
    - Now the data parameter is correctly added to the URL

--- src/bat.ajax.js
+++ src/bat.ajax.js
@@ -5,16 +5,22 @@
  *   @homepage  batjs.github.io
  *
  *   @status    beta
- *   @version   0.0.1
+ *   @version   0.0.2
  *   @author    Jo Santana
  *   @license   Released under the MIT license
  *
  *   @usage:
  *
+ *      Bat.ajax.getJSON(
+ *      {
+ *        url:  'http://www.some-rest-api.com/data',
+ *        data: { 'number': 123 }
+ *      }, function (response) { console.log(response); });
+ *
  *      Bat.ajax.getJSONP(
  *      {
  *        url:  'http://www.some-rest-api.com/data',
- *        callback: 'jsonpResponse'
+ *        callback: 'jsonpResponse',
  *        beforeSend: function (request)
  *        {
  *          request.setRequestHeader('key', 'value');
@@ -44,10 +50,23 @@
             return instance;
         },
 
+        serializeData = function (obj)
+        {
+            var data = '';
+
+            for (var key in obj) {
+                data += key + '=' + encodeURIComponent(obj[key]) + '&';
+            }
+
+            data = data.slice(0,-1);
+
+            return data;
+        },
+
         extendOptions = function (options, callback)
         {
             options.url = options.url || location.href;
-            options.data = options.data || null;
+            options.data = options.data ? serializeData(options.data) : null;
             options.beforeSend = options.beforeSend || function() {};
 
             callback = callback || function() {};
@@ -68,9 +87,10 @@
 
         sendData = function (options, callback)
         {
-            var xhttp = xhr();
+            var xhttp = xhr(),
+                uri = options.data ? (options.url + '?' + options.data) : options.url;
 
-            xhttp.open('GET', options.url, true);
+            xhttp.open('GET', uri, true);
 
             options.beforeSend(xhttp);
 
